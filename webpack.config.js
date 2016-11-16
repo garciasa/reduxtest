92223e3231d48672d10cc2cf9fd7793d2dd76e77
garcia <a.garcia@epa.ie>
2016-11-16 15:19:03 +0000

    updated packages.json and fixed some eslint errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,37 @@
-var webpack = require('webpack');
-var path = require('path');
+const path = require('path');
 const autoprefixer = require('autoprefixer');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var config = {
-	devtool: 'eval-source-map',
-  entry:[
-        './src/index.js',
-  	],
-  output:{
-    	path: path.join(__dirname, 'dist'),
-    	filename: 'bundle.js',
-      publicPath: "/"
-    },
-  module: {
-	    loaders: [
-	      {
-		      exclude: /node_modules/,
-		      loader: 'babel',
-		    },
-				{
-					test: /\.(scss|css)$/,
-					loader: ExtractTextPlugin.extract('style','css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap')
-				}
-      ]
+const config = {
+  devtool: 'eval-source-map',
+  entry: [
+    './src/index.js',
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
   },
-	sassLoader: { includePaths: [path.resolve(__dirname)]},
-	postcss: [ autoprefixer ],
-	plugins: [
-		new ExtractTextPlugin("bundle.css", { allChunks: true })
-	],
+  module: {
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+      },
+      {
+        test: /\.(scss|css)$/,
+        loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap'),
+      },
+    ],
+  },
+  sassLoader: { includePaths: [path.resolve(__dirname)] },
+  postcss: [autoprefixer],
+  plugins: [
+    new ExtractTextPlugin('bundle.css', { allChunks: true }),
+  ],
   resolve: {
-    	extensions: ['', '.scss', '.js', '.json']
-   }
-
+    extensions: ['', '.scss', '.js', '.json'],
+  },
 };
 
 module.exports = config;
